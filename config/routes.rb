c16a4e2d37a9dfe18ac23f92ed5e5364d0f6514d
Gurudath Bn <gurudath.1989@gmail.com>
2013-11-23 10:38:45 +0530

    login implementation phase 1

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ AlphabetSort::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'pagenates#index'
+   root :to => 'login#index'
 
   # See how all your routes lay out with "rake routes"
 
